0e548a6f110e504691fd7e0e4e09a318add9920b
hyunkikomori <jhs880514@naver.com>
2025-07-09 20:17:39 +0900

    Initial commit

---
+++ Source/Homework_06/Homework_06.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Homework_06.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Homework_06, "Homework_06" );
